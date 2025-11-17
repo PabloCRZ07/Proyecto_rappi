ef3658bb0b62044f37d8368c0f918c2e89699177
Pablo Cardona <pavlo.carz@gmail.com>
2025-11-16 19:25:18 -0500

    prueba

---
+++ Rappi.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Entrada y salida
+
+using namespace std;
+
+int main() {
+    cout << "Hola mundo";  // Mensaje en pantalla
+    return 0;              // Fin del programa
+}
