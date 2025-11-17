e499c26efb3d03b131bc7abc384cb579b89d9dc9
Pablo Cardona <pavlo.carz@gmail.com>
2025-11-17 13:13:38 -0500

    primera parte

--- Rappi.cpp
+++ Rappi.cpp
@@ -1,14 +1,32 @@
+// PROYECTO SISTEMA DE DOMICILIOS 
+// Juan Pablo Cardona Rodríguez
+// Samuel Felipe García Sanchez
+// Edwin Mateo Camacho Tierradentro
+
 #include <iostream>  // Entrada y salida
 
 using namespace std;
 
 int main() {
-    int telefono;
-    char nombre;
+    // variables van aquí
+    string nombre, direccion, telefono;
 
+    // Interfaz 
+        cout << "============================================\n";
+        cout << "|        BIENVENIDO A RAPPI PROYECT        | \n";
+        cout << "============================================\n";
+        cout << "| A CONTINUACION INGRESE DATOS DEL CLIENTE |\n";
+        cout << "--------------------------------------------\n";
 
-    cout << "==============================\n";
-    cout << "| BIENVENIDO A RAPPI PROYECT | \n";
-    cout << "==============================\n";
+        cout << "|- Telefono  :\n";
+        getline(cin, telefono);
+
+        cout << "|- Nombre    :\n";
+        getline(cin, nombre); 
+
+        cout << "|- Direccion :\n";
+        getline(cin, direccion); 
+
+        cout << telefono << " " << nombre << " " << direccion << "\n";
     return 0;
 }
