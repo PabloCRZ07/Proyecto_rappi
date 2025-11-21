1f0144654b3efdb657edbd1c09694e641e7eef8a
PabloCRZ07 <pavlo.carz@gmail.com>
2025-11-21 11:02:48 -0500

    Update Rappi.cpp
    21/11/25 segundo cambio var[n] guardadas

--- Rappi.cpp
+++ Rappi.cpp
@@ -9,25 +9,28 @@
 using namespace std;
 
 int main() {
-    string nombre, direccion, telefono;
+    string nombre[100], direccion[100], telefono[100];
     string repetir = "s";
+    int n = 0;
 
     cout << "============================================\n";
     cout << "|        BIENVENIDO A RAPPI PROYECT        | \n";
     cout << "============================================\n";
     cout << "| A CONTINUACION INGRESE DATOS DEL CLIENTE |\n";
     cout << "--------------------------------------------\n";
-
+    
     while (repetir == "s" || repetir == "S") {
 
         cout << "|- Telefono  : ";
-        getline(cin, telefono);
+        getline(cin, telefono[n]);
 
         cout << "|- Nombre    : ";
-        getline(cin, nombre);
+        getline(cin, nombre[n]);
 
         cout << "|- Direccion : ";
-        getline(cin, direccion);
+        getline(cin, direccion[n]);
+        
+        n++;
 
         cout << "\nDesea ingresar otro domicilio? (s/n): ";
         cin >> repetir;
@@ -36,6 +39,12 @@ int main() {
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
+	for(int i = 0; i <= n; i++){
+		cout << i + 1 << ". " 
+        << telefono[i] << " | "
+        << nombre[i]   << " | "
+        << direccion[i] << "\n";
+	}
     cout << "\nUltimo domicilio ingresado:\n";
     cout << telefono << " " << nombre << " " << direccion << "\n";
 
