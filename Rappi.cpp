435cc4192d18a87fbf6733e627713f43a76010d3
PabloCRZ07 <pavlo.carz@gmail.com>
2025-12-04 19:25:45 -0500

    Recoge todos los datos
    faltan funciones que calculan el resto de requisitos

--- Rappi.cpp
+++ Rappi.cpp
@@ -9,9 +9,10 @@
 using namespace std;
 
 int main() {
-    string nombre[100], direccion[100], telefono[100];
-    string repetir = "s";
-    int n = 0;
+    string nombre[100], direccion[100], telefono[100], producto[100];
+    float precio[100];
+    string repetir = "s", repetirDomicilio = "s";
+    int n = 0, cantidad[100];
 
     cout << "============================================\n";
     cout << "|        BIENVENIDO A RAPPI PROYECT        | \n";
@@ -19,8 +20,10 @@ int main() {
     cout << "| A CONTINUACION INGRESE DATOS DEL CLIENTE |\n";
     cout << "--------------------------------------------\n";
     
-    while (repetir == "s" || repetir == "S") {
-
+    while (repetir == "s" || repetir == "S") { 
+          
+        repetirDomicilio = "s"; 
+    
         cout << "|- Telefono  : ";
         getline(cin, telefono[n]);
 
@@ -29,6 +32,21 @@ int main() {
 
         cout << "|- Direccion : ";
         getline(cin, direccion[n]);
+        
+        while(repetirDomicilio == "s" || repetirDomicilio == "S") {
+        
+        cout << "|- Ingrese nombre del producto : \n";
+        getline(cin, producto[n]);
+        cout << "|- Ingrese cantidad : \n";
+        cin >>  cantidad[n];
+        
+        cout << "|- Ingrese precio : \n";
+        cin >> precio[n];
+        
+        cout << "Desea ingresar otro producto? (s/n): ";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // para que no se salte el getline
+        getline(cin, repetirDomicilio);
+        }
         
         n++;
 
@@ -39,14 +57,23 @@ int main() {
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-	for(int i = 0; i <= n; i++){
-		cout << i + 1 << ". " 
-        << telefono[i] << " | "
-        << nombre[i]   << " | "
-        << direccion[i] << "\n";
-	}
+    cout << "\n============================================\n";
+    cout << "        DOMICILIOS REGISTRADOS\n";
+    cout << "============================================\n";
+
+    for(int i = 0; i < n; i++){
+        cout << "\nDomicilio #" << i + 1 << "\n";
+        cout << "---------------------------------\n";
+        cout << "Telefono : " << telefono[i] << "\n";
+        cout << "Nombre   : " << nombre[i]   << "\n";
+        cout << "Direccion: " << direccion[i] << "\n";
+        cout << "Producto : " << producto[i] << "\n";
+        cout << "Cantidad : " << cantidad[i] << "\n";
+        cout << "Precio   : " << precio[i] << "\n";
+    }
     cout << "\nUltimo domicilio ingresado:\n";
     cout << telefono << " " << nombre << " " << direccion << "\n";
-
+    
+    system("pause");
     return 0;
 }
